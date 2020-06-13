e2be84174dc616880198ee2828c95dfb8240f747
Shayan Mohsin <shaymotionfilms@gmail.com>
2020-06-13 02:08:36 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,1 +1,12 @@
 // create your App component here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return ;
+  }
+  
+}
+
+ReactDOM.render(document.getElementById('app'));
